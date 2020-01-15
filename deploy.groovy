862f5bb5bded4aa4671b10b37f80a5b64d2c4ed1
dina89 <54482946+dina89@users.noreply.github.com>
2020-01-15 21:35:45 +0200

    Update deploy.groovy to build training/webapp

--- deploy.groovy
+++ deploy.groovy
@@ -1,8 +1,18 @@
 node("linux") {
-stage("Build") {
-echo "Compile package"
+def customImage = ""
+stage("create dockerfile") {
+sh """
+tee Dockerfile <<-'EOF'
+FROM ubuntu:latest
+RUN touch file-01.txt
+EOF
+"""
 }
-stage("Test") {
-echo "Test with JUnit"
+stage("build docker") {
+customImage =
+docker.build("training/webapp")
+}
+stage("verify dockers") {
+sh "docker images"
 }
 }
