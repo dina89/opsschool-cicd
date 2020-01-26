7bed0c4ccec1b7f4ad623141b36eb3875a509721
dina89 <54482946+dina89@users.noreply.github.com>
2020-01-26 21:36:56 +0200

    added checkout of webapp-deployment.yaml

--- deploy.groovy
+++ deploy.groovy
@@ -16,6 +16,16 @@ stage("verify dockers") {
 sh "docker images"
 }
 stage("deploy webapp") {
+      checkout(
+            changelog: false, 
+            poll: false, 
+            scm: [$class: 'GitSCM', 
+                  branches: [[name: '*/master']], 
+                  doGenerateSubmoduleConfigurations: false, 
+                  extensions: [[$class: 'CleanBeforeCheckout']], 
+                  submoduleCfg: [], 
+                  userRemoteConfigs: [[credentialsId: 'Github-Dina89', 
+                                       url: 'https://github.com/dina89/opsschool-cicd']]]
       kubernetesDeploy(
             configs: '**/webapp-deployment.yaml/**', 
             kubeconfigId: 'k8s_kubeconfig')
