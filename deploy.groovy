131697e5eb7473231754de2b832df3a9ed339946
dina89 <54482946+dina89@users.noreply.github.com>
2020-01-30 21:21:44 +0200

    removed pull request, changed deployemnt from local folder

--- deploy.groovy
+++ deploy.groovy
@@ -15,25 +15,13 @@ docker.build("training/webapp")
 stage("verify dockers") {
 sh "docker images"
 }
-stage("Pull deployment.yml") {
-checkout(
-            changelog: false, 
-            poll: false, 
-            scm: [$class: 'GitSCM', 
-                  branches: [[name: '*/master']], 
-                  doGenerateSubmoduleConfigurations: false, 
-                  extensions: [[$class: 'CleanBeforeCheckout']], 
-                  submoduleCfg: [], 
-                  userRemoteConfigs: [[credentialsId: 'Github-Dina89', 
-                                       url: 'https://github.com/dina89/opsschool-cicd']]])
-}
 stage("deploy webapp") {
        withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', 
                          accessKeyVariable: 'AWS_ACCESS_KEY_ID', 
                          credentialsId: 'AWS-creds', 
                          secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
             kubernetesDeploy(
-                  configs: '**/webapp-deployment.yaml/**', 
+                  configs: 'k8s/*', 
                   kubeconfigId: 'k8s_kubeconfig')
             }
       }
